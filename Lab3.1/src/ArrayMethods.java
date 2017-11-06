6e8a8c7e8ebc91d456da1e88339f7d845a11898b
Amir Hasan <ahasan0456@bths.edu>
2017-11-06 13:19:21 -0500

    Updated ArrayMethods.

--- Lab3.1/src/ArrayMethods.java
+++ Lab3.1/src/ArrayMethods.java
@@ -1,4 +1,13 @@
+/*
+*	Author: Amir Hasan
+*	Last Updated: 11-6-17
+*	Description: Lab 3.1
+*/
 public class ArrayMethods
 {
-	
+	public static int[] removeDuplicates(int[] list)
+	{
+		int[] newList;
+		return newList;
+	}
 }
