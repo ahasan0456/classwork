e753bfdcfb8205b4949d2985bde69cda0b742794
Amir Hasan <ahasan0456@bths.edu>
2017-11-08 13:21:37 -0500

    Worked on Lab 3.1.

--- Lab3.1/src/ArrayMethods.java
+++ Lab3.1/src/ArrayMethods.java
@@ -5,9 +5,44 @@
 */
 public class ArrayMethods
 {
+	public static void main(String[]args)
+	{
+		int[] list= {1,2,3,7,5,1,0,2};
+	}
 	public static int[] removeDuplicates(int[] list)
 	{
-		int[] newList;
+		
+		int[] newList=new int[list.length];
+		int i=0;
+		while(list[i]!=list[i+1])
+		{
+			newList[i]=list[i];
+			i++;
+		}
 		return newList;
+	}
+	public static int[][] productArray(int[] arr1, int[]arr2)
+	{
+		int[][]product=new int[3][4];
+		return product;
+		
+	}
+	public static int[] findDuplicates(int[] list)
+	{
+		String numList="";
+		for(int x=0;x<list.length-1;x++)
+		{
+			for(int y=x+1;y<list.length;y++)
+			{
+				if(list[x]=list[y])
+				{
+					numList=numList+x+" "+y+" ";
+				}
+			}
+		}
+	}
+	public static int[] numStringToList()
+	{
+		for()
 	}
 }
