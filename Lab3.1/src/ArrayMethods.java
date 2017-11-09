fc1ba1994f982d8bba7f74b0f3cb35296558bf67
Amir Hasan <ahasan0456@bths.edu>
2017-11-09 13:21:41 -0500

    Updated Lab 3.1.

--- Lab3.1/src/ArrayMethods.java
+++ Lab3.1/src/ArrayMethods.java
@@ -8,16 +8,34 @@ public class ArrayMethods
 	public static void main(String[]args)
 	{
 		int[] list= {1,2,3,7,5,1,0,2};
+		printArr(list);
 	}
 	public static int[] removeDuplicates(int[] list)
 	{
-		
-		int[] newList=new int[list.length];
+		/*int size=list.length;
+		 * 
 		int i=0;
 		while(list[i]!=list[i+1])
 		{
 			newList[i]=list[i];
 			i++;
+		}
+		return newList;
+		int[] newList=new int[size];
+		for(int i=0;i<list.length;i++)
+			for(int j=i+1;j<list.length-1;j++)
+			{
+				if(list[i]!=list[j])
+				{
+					newList[i]=list[i];
+				}
+			}
+		*/
+		int[] dupePosList=returnDuplicatePos(list);
+		int[] newList=new int[list.length-dupePosList.length];
+		for(x=0;x<newList.length;x++)
+		{
+			
 		}
 		return newList;
 	}
@@ -27,22 +45,57 @@ public class ArrayMethods
 		return product;
 		
 	}
-	public static int[] findDuplicates(int[] list)
+	private static void printArr(int[]list)
 	{
-		String numList="";
+		for(int item: list)
+			System.out.print(item);
+	}
+	private static boolean contains(int[]numList, int target)
+	{
+		for(int num:numList)
+		{
+			if(numList[num]==target)
+				return true;
+		}
+	}
+	public static int[] returnDuplicatePos(int[] list)
+	{
+		int count=0;
+		boolean xDupe=false;
+		int[] countedPos=new int[list.length];
 		for(int x=0;x<list.length-1;x++)
 		{
 			for(int y=x+1;y<list.length;y++)
 			{
-				if(list[x]=list[y])
+				if(list[x]==list[y])
 				{
-					numList=numList+x+" "+y+" ";
+					for(int z=0;z<countedPos.length;z++)
+					{
+						if(list[x]==countedPos[z])
+						{
+							xDupe=true;
+						}
+					}
+					if(xDupe)
+					{
+						countedPos[count]=y;
+						count++;
+					}
+					else
+					{
+						countedPos[count]=x;
+						countedPos[count]=y;
+						count+=2;
+					}
+					xDupe=false;
 				}
 			}
 		}
-	}
-	public static int[] numStringToList()
-	{
-		for()
+		int[] newCountPos=new int[count];
+		for(int c=0;c<count;c++)
+		{
+			newCountPos[c]=countedPos[c];
+		}
+		return newCountPos;
 	}
 }
