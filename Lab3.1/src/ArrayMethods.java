54d00eef3701b4aad2c52abb346aa6e172ce2c75
Amir Hasan <ahasan0456@bths.edu>
2017-11-13 13:21:03 -0500

    Updated Lab 3.1, added warm-up for 11-13.

--- Lab3.1/src/ArrayMethods.java
+++ Lab3.1/src/ArrayMethods.java
@@ -7,48 +7,61 @@ public class ArrayMethods
 {
 	public static void main(String[]args)
 	{
-		int[] list= {1,2,3,7,5,1,0,2};
+		int[] list= {1,2,3,7,5,1,0,1,2,1,0,5};
+		int[] list1= {1,3,5};
+		int[] list2= {};
 		printArr(list);
+		printArr(removeDuplicates(list));
 	}
 	public static int[] removeDuplicates(int[] list)
 	{
-		/*int size=list.length;
-		 * 
-		int i=0;
-		while(list[i]!=list[i+1])
-		{
-			newList[i]=list[i];
-			i++;
-		}
-		return newList;
-		int[] newList=new int[size];
+		
+		int size=0;
+		int[] newList=new int[list.length];
 		for(int i=0;i<list.length;i++)
+		{
+			boolean unique=true;
 			for(int j=i+1;j<list.length-1;j++)
 			{
-				if(list[i]!=list[j])
+				if(list[i]==list[j])
 				{
-					newList[i]=list[i];
+					unique=false;
+					break;
 				}
 			}
-		*/
-		int[] dupePosList=returnDuplicatePos(list);
-		int[] newList=new int[list.length-dupePosList.length];
-		for(x=0;x<newList.length;x++)
-		{
-			
+			if(unique)
+			{
+				newList[size]=list[i];
+				size++;
+			}
 		}
-		return newList;
+		int[] newNewList=new int[size];
+		for(int x=0;x<size;x++)
+		{
+			newNewList[x]=newList[x];
+		}
+		return newNewList;
 	}
 	public static int[][] productArray(int[] arr1, int[]arr2)
 	{
-		int[][]product=new int[3][4];
+		int[][]product=new int[arr1.length][arr2.length];
+		for(int x=0;x<product.length;x++)
+		{
+			for(int y=0;y<product[x].length;y++)
+			{
+				product[x][y]=arr1[x]*arr2[y];
+			}
+		}
 		return product;
 		
 	}
 	private static void printArr(int[]list)
 	{
 		for(int item: list)
-			System.out.print(item);
+		{
+			System.out.print("["+item+"]");
+		}
+			System.out.println();
 	}
 	private static boolean contains(int[]numList, int target)
 	{
@@ -57,11 +70,11 @@ public class ArrayMethods
 			if(numList[num]==target)
 				return true;
 		}
+		return false;
 	}
 	public static int[] returnDuplicatePos(int[] list)
 	{
 		int count=0;
-		boolean xDupe=false;
 		int[] countedPos=new int[list.length];
 		for(int x=0;x<list.length-1;x++)
 		{
@@ -69,25 +82,8 @@ public class ArrayMethods
 			{
 				if(list[x]==list[y])
 				{
-					for(int z=0;z<countedPos.length;z++)
-					{
-						if(list[x]==countedPos[z])
-						{
-							xDupe=true;
-						}
-					}
-					if(xDupe)
-					{
-						countedPos[count]=y;
-						count++;
-					}
-					else
-					{
-						countedPos[count]=x;
-						countedPos[count]=y;
-						count+=2;
-					}
-					xDupe=false;
+					countedPos[count]=y;
+					count++;
 				}
 			}
 		}
