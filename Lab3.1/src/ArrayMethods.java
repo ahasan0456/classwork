0de0391a10f36e75253f2d65da50e6d247099882
Amir Hasan <ahasan0456@bths.edu>
2017-11-14 13:21:00 -0500

    Made Pascal Triangle.

--- Lab3.1/src/ArrayMethods.java
+++ Lab3.1/src/ArrayMethods.java
@@ -1,21 +1,35 @@
 /*
-*	Author: Amir Hasan
-*	Last Updated: 11-6-17
+*	Authors: Amir Hasan & Justin Fagan
+*	Last Updated: 11-14-17
 *	Description: Lab 3.1
 */
 public class ArrayMethods
 {
 	public static void main(String[]args)
 	{
-		int[] list= {1,2,3,7,5,1,0,1,2,1,0,5};
-		int[] list1= {1,3,5};
-		int[] list2= {};
+		int[] list= {1,2,3,7,5,1,0,1,2,1,0,9,5};
+		int[] list1= {1,2,3};
+		int[] list2= {4,3,2};
 		printArr(list);
+		System.out.println();
 		printArr(removeDuplicates(list));
+		System.out.println();
+		int[][] list12product=productArray(list1,list2);
+		for(int x=0;x<list12product.length;x++)
+		{
+			printArr(list12product[x]);
+			System.out.println();
+		}
+		int[][] pascalTri=pascalTriangle(5);
+		for(int x=0;x<pascalTri.length;x++)
+		{
+			printArr(pascalTri[x]);
+			System.out.println();
+		}
+		printPascalTriangle(pascalTri);
 	}
 	public static int[] removeDuplicates(int[] list)
 	{
-		
 		int size=0;
 		int[] newList=new int[list.length];
 		for(int i=0;i<list.length;i++)
@@ -49,11 +63,30 @@ public class ArrayMethods
 		{
 			for(int y=0;y<product[x].length;y++)
 			{
-				product[x][y]=arr1[x]*arr2[y];
+				product[x][y]=arr1[y]*arr2[x];
 			}
 		}
 		return product;
-		
+	}
+	public static int[][] pascalTriangle(int n)
+	{
+		int[][] pascalTriangle=new int[n][];
+		for(int x=0;x<n;x++)
+		{
+			pascalTriangle[x]=new int[x+1];
+			for(int y=0;y<pascalTriangle[x].length;y++)
+			{
+				if(y==x||y==0)
+				{
+					pascalTriangle[x][y]=1;
+				}
+				else
+				{
+					pascalTriangle[x][y]=pascalTriangle[x-1][y-1]+pascalTriangle[x-1][y];
+				}
+			}
+		}
+		return pascalTriangle;
 	}
 	private static void printArr(int[]list)
 	{
@@ -61,37 +94,24 @@ public class ArrayMethods
 		{
 			System.out.print("["+item+"]");
 		}
+	}
+	public static void printPascalTriangle(int[][] pTriangle)
+	{
+		/*for(int x=0;x<pTriangle.length;x++)
+		{
+			printArr(pTriangle[x]);
 			System.out.println();
-	}
-	private static boolean contains(int[]numList, int target)
-	{
-		for(int num:numList)
+		}*/
+		for(int x=0;x<pTriangle.length;x++)
 		{
-			if(numList[num]==target)
-				return true;
-		}
-		return false;
-	}
-	public static int[] returnDuplicatePos(int[] list)
-	{
-		int count=0;
-		int[] countedPos=new int[list.length];
-		for(int x=0;x<list.length-1;x++)
-		{
-			for(int y=x+1;y<list.length;y++)
+			System.out.print(" ");
+			for(int y=0;y<pTriangle[x].length;y++)
 			{
-				if(list[x]==list[y])
-				{
-					countedPos[count]=y;
-					count++;
-				}
+				System.out.print(pTriangle[x][y]);
+				System.out.print(" ");
 			}
+			System.out.print(" ");
+			System.out.println();
 		}
-		int[] newCountPos=new int[count];
-		for(int c=0;c<count;c++)
-		{
-			newCountPos[c]=countedPos[c];
-		}
-		return newCountPos;
 	}
 }
