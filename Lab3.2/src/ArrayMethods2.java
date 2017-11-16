4afc491157fd4bcab5efb729d62afb73ce9f2f5c
Amir Hasan <ahasan0456@bths.edu>
2017-11-16 13:21:50 -0500

    Updated Lab 3.2.

--- Lab3.2/src/ArrayMethods2.java
+++ Lab3.2/src/ArrayMethods2.java
@@ -1,18 +1,33 @@
+/*
+*	Author: Amir Hasan & Justin Fagan
+*	Last Updated: 11-16-17
+*	Description: Lab 3.2
+*/
 public class ArrayMethods2
 {
 	public static void main(String[]args)
 	{
-		
+		printArr(merge(args, args));
+	}
+	private static void printArr(String[] list) {
+		for(String h:list)
+			System.out.print(list[i]);
 	}
 	public static String[] merge(String[]list1,String[]list2)
 	{
 		int size= list1.length+list2.length;
 		String[]result=new String[size];
-		for(int i=0;i<list2.length;i++)
-			for(int j=0;j<list1.length;j++)
-			{
-				
-			}
+		String add;
+		for(int j=0;j<list1.length;j++)
+		{
+			result[j]=list1[j];
+		}
+		for(int k=list1.length+1;k<size;k++)
+		{
+			result[k]=list2[k-4];
+		}
+		//result[i]=add;
+		return result;
 	}
 	public static String[] mergeSort(String[] list)
 	{
