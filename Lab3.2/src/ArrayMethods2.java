a0f43af73098754f4fafee6957a469047e8c4e3a
Amir Hasan <ahasan0456@bths.edu>
2017-11-20 13:21:08 -0500

    Updated Lab 3.2.

--- Lab3.2/src/ArrayMethods2.java
+++ Lab3.2/src/ArrayMethods2.java
@@ -11,11 +11,24 @@ public class ArrayMethods2
 		String[] test2= {"Alphabet","Cooking","Eating","Sleeping"};
 		String[] merged=merge(test1,test2);
 		printArr(merged);
-		int[] test3= {6,2,8,4,6,0,1,7};
-		System.out.println(partition(test3));
+		String[] test4= {"Nested","Quickly","Array","Match"};
+		String[] mergeSorted=mergeSort(test4);
+		printArr(mergeSorted);
+		int[] test3= {6,2,8,4,6,0,1,7};//Should return 5	
+		System.out.println(partition(test3)); //Should be {2,4,6,0,1,6,8,7}
+		printArr(test3);
+		int[] test5= {0,1,6,5,4,2,7};//Should return 0
+		System.out.println(partition(test5));
 	}
 	private static void printArr(String[] list) {
 		for(String h:list)
+		{
+			System.out.print("["+h+"] ");
+		}
+		System.out.println();
+	}
+	private static void printArr(int[] list) {
+		for(int h:list)
 		{
 			System.out.print("["+h+"] ");
 		}
@@ -61,21 +74,70 @@ public class ArrayMethods2
 	{
 		if(list.length>1)
 		{
-			mergeSort()
+			String[] list1=new String[list.length/2];
+			String[] list2=new String[list.length-list1.length];
+			int z=0;
+			for(int x=0;x<list1.length;x++)
+			{
+				list1[x]=list[x];
+				z=x;
+			}
+			for(int y=0;y<list2.length;y++)
+			{
+				list2[y]=list[z];
+			}
+			String[] sort1=mergeSort(list1);
+			String[] sort2=mergeSort(list2);
+			String[] newList=merge(sort1,sort2);
+			return newList;
+		}
+		else
+		{
+			return list;
 		}
 	}
 	public static int partition(int[] list)
 	{
-		int[] newList=new int[list.length];
-		int tracker=0;
-		int pivotNum=list[0];
-		for(int i=0;i<=list.length;i++)
+		/*boolean sorted=false;
+		for(int x=0;x<list.length-1;x++) //checks if list is sorted
 		{
-			if(list[i]<=pivotNum)
+			if(list[x]<=list[x+1])
+				sorted=true;
+		}
+		while(!sorted)
+		{
+			int tracker=0;
+			int pivotNum=list[tracker];
+			for(int i=0;i<list.length;i++)
 			{
-				
+				if(list[i]<=pivotNum)
+				{
+					swap(list, i,i+1);
+					tracker++;
+				}
 			}
 		}
-		return -1;
+		return tracker;*/
+		int tracker=0;
+		int pivotNum=list[0];
+		for(int i=0;i<list.length;i++)
+		{
+			if(list[i]<=pivotNum&&i>0)
+			{
+				for(int j=tracker+1;j<(i-tracker);j++)
+				{
+					swap(list,j,j+1);
+				}
+				swap(list,tracker,tracker+1);
+				tracker++;
+			}
+		}
+		return tracker;
+	}
+	public static void swap(int arr[],int i, int j)
+	{
+		int x=arr[i];
+		arr[i]=arr[j];
+		arr[j]=x;
 	}
 }
