3ded68405871a0f1db73ec7f5fd835d582a13fec
Amir Hasan <ahasan0456@bths.edu>
2017-11-27 13:07:36 -0500

    Added runtime testing on Lab 3.3.

--- Lab3.3/src/ArrayMethods3.java
+++ Lab3.3/src/ArrayMethods3.java
@@ -7,15 +7,30 @@ public class ArrayMethods3
 {
 	public static void main(String[] args)
 	{
-		String[] testBubble= {"Cheese","Apple","Grape","Jelly","Beef","Apple"};
-		bubbleSort(testBubble);
-		printArr(testBubble);
-		int[] testInsertion= {5,7,2,3,5,0,8,1,9};
-		insertionSort(testInsertion);
-		printArr(testInsertion);
-		double[] testSelection= {5.7,6.8,2.3,0.8,1.9,6.8};
-		selectionSort(testSelection);
-		printArr(testSelection);
+		int[] testTime= {5,6,3,2,8,7,4,6,3,1,4,3,0,9};
+		long startTime=System.nanoTime();
+		bubbleSort(testTime);
+		long endTime=System.nanoTime();
+		long totalTime=endTime-startTime;
+		double secondTime=totalTime/1000;
+		printArr(testTime);
+		System.out.println("Time taken by bubble sort: "+secondTime);
+		int[] testTimeTwo= {5,6,3,2,8,7,4,6,3,1,4,3,0,9};
+		startTime=System.nanoTime();
+		selectionSort(testTimeTwo);
+		endTime=System.nanoTime();
+		totalTime=endTime-startTime;
+		secondTime=totalTime/1000;
+		printArr(testTimeTwo);
+		System.out.println("Time taken by selection sort: "+secondTime);
+		int[] testTimeThree= {5,6,3,2,8,7,4,6,3,1,4,3,0,9};
+		startTime=System.nanoTime();
+		insertionSort(testTimeThree);
+		endTime=System.nanoTime();
+		totalTime=endTime-startTime;
+		secondTime=totalTime/1000;
+		printArr(testTime);
+		System.out.println("Time taken by insertion sort: "+secondTime);
 	}
 	public static void insertionSort(int[] list1)
 	{
@@ -52,6 +67,28 @@ public class ArrayMethods3
 			}
 		}
 	}
+	public static void selectionSort(int[] list1)
+	{
+		int lowestNumPos;
+		int lowestNum;
+		for(int x=0;x<list1.length-1;x++)
+		{
+			lowestNumPos=x;
+			lowestNum=list1[x];
+			for(int y=x+1;y<list1.length;y++)
+			{
+				if(list1[y]<lowestNum)
+				{
+					lowestNumPos=y;
+					lowestNum=list1[y];
+				}
+			}
+			if(lowestNumPos>x)
+			{
+				swap(list1,x,lowestNumPos);
+			}
+		}
+	}
 	public static void bubbleSort(String[] list1)
 	{
 		boolean sorted=false;
@@ -61,6 +98,30 @@ public class ArrayMethods3
 			for(int x=0;x<list1.length-1;x++)
 			{
 				if(list1[x].compareTo(list1[x+1])>0)
+				{
+					swap(list1,x,x+1);
+					swapped=true;
+				}
+			}
+			if(!swapped)
+			{
+				sorted=true;
+			}
+			else
+			{
+				swapped=false;
+			}
+		}
+	}
+	public static void bubbleSort(int[] list1)
+	{
+		boolean sorted=false;
+		boolean swapped=false;
+		while(!sorted)
+		{
+			for(int x=0;x<list1.length-1;x++)
+			{
+				if(list1[x]>list1[x+1])
 				{
 					swap(list1,x,x+1);
 					swapped=true;
--- Lab3.4/src/ArrayMethods4.java
+++ Lab3.4/src/ArrayMethods4.java
@@ -5,5 +5,29 @@
 */
 public class ArrayMethods4
 {
-	
+	public static void main(String[] args)
+	{
+		int[] test1= {0,1,2,3,4,5};
+		int[] test2= {6,4,12,8,10};
+		int[] test3= {4,2,5,7,4,2,5,9,1};
+	}
+	public static void partition(int[] list1, int front, int back)
+	{
+		int pivotNum=list1[front];
+		int tracker=front;
+	}
+	public static void quickSort(int[] list1, int front, int back)
+	{
+		
+	}
+	public static void customSort()
+	{
+		
+	}
+	public static void swap(int arr[],int i, int j)
+	{
+		int x=arr[i];
+		arr[i]=arr[j];
+		arr[j]=x;
+	}
 }
