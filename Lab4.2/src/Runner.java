a929abd4940f6a60836062ac1cfdbbd5739b5328
Amir Hasan <ahasan0456@bths.edu>
2017-12-22 12:42:06 -0500

    Implemented getColumnHeaders.

--- Lab4.2/src/Runner.java
+++ Lab4.2/src/Runner.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.util.List;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
@@ -9,7 +10,7 @@ import javafx.stage.Stage;
 import javafx.scene.chart.CategoryAxis;
 /*
 *	Author: Amir Hasan
-*	Last Updated: 12-21-2017
+*	Last Updated: 12-22-2017
 *	Description: Lab 4.2 - Runner to read a csv file.
 */
 public class Runner extends Application
@@ -23,6 +24,7 @@ public class Runner extends Application
 	{
 		File dataset=new File("New_York_City_Population_by_Borough__1950_-_2040.csv");
 		CSVUtilities data=new CSVUtilities(dataset);
+		List<String> headers=data.getColumnHeaders();
 		int firstCol=2;
 		stage.setTitle("Population of NYC");
 		final CategoryAxis xAxis = new CategoryAxis();
@@ -33,52 +35,52 @@ public class Runner extends Application
         lineChart.setTitle("Census Data for NYC Population");
         Series<String, Number> nyc = new XYChart.Series();
         nyc.setName("NYC");
-        nyc.getData().add(new XYChart.Data("1950", data.getDataInt(firstCol).get(0)));
-        nyc.getData().add(new XYChart.Data("1960", data.getDataInt(firstCol+2).get(0)));
-        nyc.getData().add(new XYChart.Data("1970", data.getDataInt(firstCol+4).get(0)));
-        nyc.getData().add(new XYChart.Data("1980", data.getDataInt(firstCol+6).get(0)));
-        nyc.getData().add(new XYChart.Data("1990", data.getDataInt(firstCol+8).get(0)));
-        nyc.getData().add(new XYChart.Data("2000", data.getDataInt(firstCol+10).get(0)));
+        nyc.getData().add(new XYChart.Data(headers.get(firstCol), data.getDataInt(firstCol).get(0)));
+        nyc.getData().add(new XYChart.Data(headers.get(firstCol+2), data.getDataInt(firstCol+2).get(0)));
+        nyc.getData().add(new XYChart.Data(headers.get(firstCol+4), data.getDataInt(firstCol+4).get(0)));
+        nyc.getData().add(new XYChart.Data(headers.get(firstCol+6), data.getDataInt(firstCol+6).get(0)));
+        nyc.getData().add(new XYChart.Data(headers.get(firstCol+8), data.getDataInt(firstCol+8).get(0)));
+        nyc.getData().add(new XYChart.Data(headers.get(firstCol+10), data.getDataInt(firstCol+10).get(0)));
         Series<String, Number> bronx = new XYChart.Series();
         bronx.setName("Bronx");
-        bronx.getData().add(new XYChart.Data("1950", data.getDataInt(firstCol).get(1)));
-        bronx.getData().add(new XYChart.Data("1960", data.getDataInt(firstCol+2).get(1)));
-        bronx.getData().add(new XYChart.Data("1970", data.getDataInt(firstCol+4).get(1)));
-        bronx.getData().add(new XYChart.Data("1980", data.getDataInt(firstCol+6).get(1)));
-        bronx.getData().add(new XYChart.Data("1990", data.getDataInt(firstCol+8).get(1)));
-        bronx.getData().add(new XYChart.Data("2000", data.getDataInt(firstCol+10).get(1)));
+        bronx.getData().add(new XYChart.Data(headers.get(firstCol), data.getDataInt(firstCol).get(1)));
+        bronx.getData().add(new XYChart.Data(headers.get(firstCol+2), data.getDataInt(firstCol+2).get(1)));
+        bronx.getData().add(new XYChart.Data(headers.get(firstCol+4), data.getDataInt(firstCol+4).get(1)));
+        bronx.getData().add(new XYChart.Data(headers.get(firstCol+6), data.getDataInt(firstCol+6).get(1)));
+        bronx.getData().add(new XYChart.Data(headers.get(firstCol+8), data.getDataInt(firstCol+8).get(1)));
+        bronx.getData().add(new XYChart.Data(headers.get(firstCol+10), data.getDataInt(firstCol+10).get(1)));
         Series<String, Number> brooklyn = new XYChart.Series();
         brooklyn.setName("Brooklyn");
-        brooklyn.getData().add(new XYChart.Data("1950", data.getDataInt(firstCol).get(2)));
-        brooklyn.getData().add(new XYChart.Data("1960", data.getDataInt(firstCol+2).get(2)));
-        brooklyn.getData().add(new XYChart.Data("1970", data.getDataInt(firstCol+4).get(2)));
-        brooklyn.getData().add(new XYChart.Data("1980", data.getDataInt(firstCol+6).get(2)));
-        brooklyn.getData().add(new XYChart.Data("1990", data.getDataInt(firstCol+8).get(2)));
-        brooklyn.getData().add(new XYChart.Data("2000", data.getDataInt(firstCol+10).get(2)));
+        brooklyn.getData().add(new XYChart.Data(headers.get(firstCol), data.getDataInt(firstCol).get(2)));
+        brooklyn.getData().add(new XYChart.Data(headers.get(firstCol+2), data.getDataInt(firstCol+2).get(2)));
+        brooklyn.getData().add(new XYChart.Data(headers.get(firstCol+4), data.getDataInt(firstCol+4).get(2)));
+        brooklyn.getData().add(new XYChart.Data(headers.get(firstCol+6), data.getDataInt(firstCol+6).get(2)));
+        brooklyn.getData().add(new XYChart.Data(headers.get(firstCol+8), data.getDataInt(firstCol+8).get(2)));
+        brooklyn.getData().add(new XYChart.Data(headers.get(firstCol+10), data.getDataInt(firstCol+10).get(2)));
         Series<String, Number> manhattan = new XYChart.Series();
         manhattan.setName("Manhattan");
-        manhattan.getData().add(new XYChart.Data("1950", data.getDataInt(firstCol).get(3)));
-        manhattan.getData().add(new XYChart.Data("1960", data.getDataInt(firstCol+2).get(3)));
-        manhattan.getData().add(new XYChart.Data("1970", data.getDataInt(firstCol+4).get(3)));
-        manhattan.getData().add(new XYChart.Data("1980", data.getDataInt(firstCol+6).get(3)));
-        manhattan.getData().add(new XYChart.Data("1990", data.getDataInt(firstCol+8).get(3)));
-        manhattan.getData().add(new XYChart.Data("2000", data.getDataInt(firstCol+10).get(3)));
+        manhattan.getData().add(new XYChart.Data(headers.get(firstCol), data.getDataInt(firstCol).get(3)));
+        manhattan.getData().add(new XYChart.Data(headers.get(firstCol+2), data.getDataInt(firstCol+2).get(3)));
+        manhattan.getData().add(new XYChart.Data(headers.get(firstCol+4), data.getDataInt(firstCol+4).get(3)));
+        manhattan.getData().add(new XYChart.Data(headers.get(firstCol+6), data.getDataInt(firstCol+6).get(3)));
+        manhattan.getData().add(new XYChart.Data(headers.get(firstCol+8), data.getDataInt(firstCol+8).get(3)));
+        manhattan.getData().add(new XYChart.Data(headers.get(firstCol+10), data.getDataInt(firstCol+10).get(3)));
         Series<String, Number> queens = new XYChart.Series();
         queens.setName("Queens");
-        queens.getData().add(new XYChart.Data("1950", data.getDataInt(firstCol).get(4)));
-        queens.getData().add(new XYChart.Data("1960", data.getDataInt(firstCol+2).get(4)));
-        queens.getData().add(new XYChart.Data("1970", data.getDataInt(firstCol+4).get(4)));
-        queens.getData().add(new XYChart.Data("1980", data.getDataInt(firstCol+6).get(4)));
-        queens.getData().add(new XYChart.Data("1990", data.getDataInt(firstCol+8).get(4)));
-        queens.getData().add(new XYChart.Data("2000", data.getDataInt(firstCol+10).get(4)));
+        queens.getData().add(new XYChart.Data(headers.get(firstCol), data.getDataInt(firstCol).get(4)));
+        queens.getData().add(new XYChart.Data(headers.get(firstCol+2), data.getDataInt(firstCol+2).get(4)));
+        queens.getData().add(new XYChart.Data(headers.get(firstCol+4), data.getDataInt(firstCol+4).get(4)));
+        queens.getData().add(new XYChart.Data(headers.get(firstCol+6), data.getDataInt(firstCol+6).get(4)));
+        queens.getData().add(new XYChart.Data(headers.get(firstCol+8), data.getDataInt(firstCol+8).get(4)));
+        queens.getData().add(new XYChart.Data(headers.get(firstCol+10), data.getDataInt(firstCol+10).get(4)));
         Series<String, Number> statenIsland = new XYChart.Series();
         statenIsland.setName("Staten Island");
-        statenIsland.getData().add(new XYChart.Data("1950", data.getDataInt(firstCol).get(5)));
-        statenIsland.getData().add(new XYChart.Data("1960", data.getDataInt(firstCol+2).get(5)));
-        statenIsland.getData().add(new XYChart.Data("1970", data.getDataInt(firstCol+4).get(5)));
-        statenIsland.getData().add(new XYChart.Data("1980", data.getDataInt(firstCol+6).get(5)));
-        statenIsland.getData().add(new XYChart.Data("1990", data.getDataInt(firstCol+8).get(5)));
-        statenIsland.getData().add(new XYChart.Data("2000", data.getDataInt(firstCol+10).get(5)));
+        statenIsland.getData().add(new XYChart.Data(headers.get(firstCol), data.getDataInt(firstCol).get(5)));
+        statenIsland.getData().add(new XYChart.Data(headers.get(firstCol+2), data.getDataInt(firstCol+2).get(5)));
+        statenIsland.getData().add(new XYChart.Data(headers.get(firstCol+4), data.getDataInt(firstCol+4).get(5)));
+        statenIsland.getData().add(new XYChart.Data(headers.get(firstCol+6), data.getDataInt(firstCol+6).get(5)));
+        statenIsland.getData().add(new XYChart.Data(headers.get(firstCol+8), data.getDataInt(firstCol+8).get(5)));
+        statenIsland.getData().add(new XYChart.Data(headers.get(firstCol+10), data.getDataInt(firstCol+10).get(5)));
         Scene scene=new Scene(lineChart,800,600);
         lineChart.getData().add(nyc);
         lineChart.getData().add(bronx);
